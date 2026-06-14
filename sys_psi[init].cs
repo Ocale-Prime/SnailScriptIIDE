8731f7e038ecbec091f015cedcc1c34937c7085e
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-14 21:27:40 +0200

    Add initial implementation of system classes + PSI initialization

---
+++ sys_psi[init].cs
@@ -0,0 +1,25 @@
+using System;
+using System.Formats.Asn1;
+
+namespace classes_main()
+{
+    class system_local
+    {
+        static void system()
+        {
+            var functoin_32 = new system_init_Psi();
+            functoin_32.load_init();
+
+            functoin_32.load_psi();   
+        }
+    }
+
+  class system_global : system_local
+  {
+      readonly struct system_local
+      {
+          var function = new func32();
+          function.load_psi();
+      }
+  }
+}
