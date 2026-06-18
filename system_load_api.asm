483852e4eb0a990e0f789751916d7262006c516c
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-18 22:13:17 +0200

    Add initial implementation of system_load_api.asm

---
+++ system_load_api.asm
@@ -0,0 +1,10 @@
+; Create local Hardwrae framework, (C) Ocale
+
+.start:
+  cmp dx, 4
+  cmp r0, 34
+  mov rdx, 4
+  D003: msg "Ld:: Create idstd::loader(){} for the HKEY[local machine function]", db 0x3200f$-equ
+  ret 0
+
+  ret 0
